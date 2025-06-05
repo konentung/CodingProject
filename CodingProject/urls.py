fe6e1f997cf6b9fb874aaca4ac918fbfc51e40f1
konentung <84655730+konentung@users.noreply.github.com>
2025-06-05 14:21:39 +0800

    bugfix static路徑異常

--- CodingProject/urls.py
+++ CodingProject/urls.py
@@ -48,3 +48,10 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+else:
+    from django.views.static import serve
+    urlpatterns += [
+        path('media/<path:path>/', serve, {'document_root': settings.MEDIA_ROOT}),
+        path('static/<path:path>/', serve, {'document_root': settings.STATIC_ROOT}),
+    ]
